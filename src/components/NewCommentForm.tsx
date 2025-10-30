d53ed8129cfb8de67903dd56620db907dcf23758
Mykola Kuzmenko <kolua.kuzmenko@gmail.com>
2025-10-30 13:55:48 +0200

    solution

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,81 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { CommentData } from '../types/Comment';
 
-export const NewCommentForm: React.FC = () => {
+// 1. Создать отслеживание состояние формы
+// 2. Создать отслеживание состояния ошибок
+// 3. Создать обработчик сабмита на форму -> Проверить валидность пришедщих данных (не пустые ли)
+// 3.1 Если не прошли то выдать ошибку (с подсвечиванием полей где не прошли).Задать инпутам класс ошибки и описание ошибки
+// 3.2 Если все гуд, то отправляем форму с помощью переданного хендлера из PostDetails (он нужен там, для обработки ошибки)
+// 4. После отправки очистить body
+// 5. При клике на clear очистить всю формы ( и Ероры )
+// 6. При сабмите на кнопку Add добавить класс loading
+
+type Props = {
+  OnFormSubmit: (data: CommentData) => void;
+  loading: boolean;
+};
+export const NewCommentForm: React.FC<Props> = ({ OnFormSubmit, loading }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    body: '',
+  });
+
+  const [errorForm, setErrorForm] = useState({
+    name: false,
+    email: false,
+    body: false,
+  });
+
+  function handleSubmitForm(e: React.FormEvent) {
+    e.preventDefault();
+
+    const errors = {
+      name: !formData.name.trim(),
+      email: !formData.email.trim(),
+      body: !formData.body.trim(),
+    };
+
+    if (Object.values(errors).some(error => error)) {
+      setErrorForm(errors);
+
+      return;
+    }
+
+    setFormData(prev => ({ ...prev, body: '' }));
+    OnFormSubmit(formData);
+  }
+
+  function handleFormChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+
+    setErrorForm({
+      ...errorForm,
+      [event.target.name]: false,
+    });
+  }
+
+  function handleClearForm() {
+    setFormData({
+      name: '',
+      email: '',
+      body: '',
+    });
+    setErrorForm({
+      name: false,
+      email: false,
+      body: false,
+    });
+  }
+
   return (
-    <form data-cy="NewCommentForm">
+    <form data-cy="NewCommentForm" onSubmit={handleSubmitForm}>
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -13,25 +86,33 @@ export const NewCommentForm: React.FC = () => {
             type="text"
             name="name"
             id="comment-author-name"
+            value={formData.name}
+            onChange={handleFormChange}
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames('input', {
+              'is-danger': errorForm.name,
+            })}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {errorForm.name && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {errorForm.name && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -45,24 +126,32 @@ export const NewCommentForm: React.FC = () => {
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames('input', {
+              'is-danger': errorForm.email,
+            })}
+            value={formData.email}
+            onChange={handleFormChange}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {errorForm.email && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {errorForm.email && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -75,25 +164,40 @@ export const NewCommentForm: React.FC = () => {
             id="comment-body"
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames('textarea', {
+              'is-danger': errorForm.body,
+            })}
+            onChange={handleFormChange}
+            value={formData.body}
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {errorForm.body && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames('button', 'is-link', {
+              'is-loading': loading,
+            })}
+          >
             Add
           </button>
         </div>
 
         <div className="control">
           {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            onClick={handleClearForm}
+          >
             Clear
           </button>
         </div>
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { User } from '../types/User';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  users: User[];
+  selectedUserId: number | null;
+  onUserIdSelector: (userId: number) => void;
+  onPosts: (userId: number) => void;
+};
+export const UserSelector: React.FC<Props> = ({
+  users,
+  selectedUserId,
+  onUserIdSelector,
+  onPosts,
+}) => {
+  const [isOpenSelector, setIsOpenSelector] = useState(false);
+  const [nameSelectedUser, setNameSelectedUser] = useState('Choose a user');
+
+  function handlerUserItemClick(userId: number, userName: string) {
+    onUserIdSelector(userId);
+    setIsOpenSelector(prev => !prev);
+    setNameSelectedUser(userName);
+    onPosts(userId);
+  }
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
+    <div
+      data-cy="UserSelector"
+      className={classNames('dropdown', {
+        'is-active': isOpenSelector,
+      })}
+    >
       <div className="dropdown-trigger">
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setIsOpenSelector(prev => !prev)}
         >
-          <span>Choose a user</span>
+          <span>{nameSelectedUser}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +49,20 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {users.map(user => {
+            return (
+              <a
+                href={`#user-${user.id}`}
+                className={classNames('dropdown-item', {
+                  'is-active': user.id === selectedUserId,
+                })}
+                key={user.id}
+                onClick={() => handlerUserItemClick(user.id, user.name)}
+              >
+                {user.name}
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
